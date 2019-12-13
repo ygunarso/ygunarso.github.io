5ad9ad728d1f163865549b286208ff0f9d1be9a0
Steven Gunarso <ygunarso@stevens.edu>
2019-12-13 00:39:55 -0500

    Test php script and google cloud

--- Android/includes/Constants.php
+++ Android/includes/Constants.php
@@ -2,4 +2,4 @@
     define('DB_NAME', 'android');
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'Loveislife1!');
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', '34.73.180.207');
