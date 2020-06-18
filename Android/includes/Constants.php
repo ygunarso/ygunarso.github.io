bf9871c2e3e466ee2bceb86bd04eabb10d4dda55
Steven Gunarso <ygunarso@stevens.edu>
2020-06-18 15:32:49 -0700

    Revamped portfolio, more projects to come soon

--- Android/includes/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('DB_NAME', 'android');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'Loveislife1!');
-    define('DB_HOST', '34.73.180.207');
---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.menu-toggle').on('click', function () {
+        $(this).toggleClass('open');
+        $('.nav').toggleClass('open');
+    });
+
+    $('.nav .nav-link').on('click', function () {
+        $('.menu-toggle').removeClass('open');
+        $('.nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
